c856d52c10599d46c5330cdb1bfef64e36a98bfa
dgabridb3 <dgabridb3@gmail.com>
2022-02-16 12:56:53 -0300

    foi feito os cards

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ display: grid;
 grid-template-columns: 1fr 3fr 1fr;
 background-color: blueviolet;
 color: white;
-height: 100vh;
+padding-bottom: 20px
 `
 
 const Div = styled.div`
@@ -16,8 +16,47 @@ border: 1px solid white;
 margin: 3px;
 `
 
-class App extends React.Component {
+/* inicio estilização card */
+const Div1 = styled.div`
+display:flex;
+align-items: center;
+justify-content: space-between;
+`
+const CardSection = styled.div `
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 16px;
+    
+`
+const Card = styled.div `
+border: 1px solid white;
+width: 100%;
+height: 100%;
+`
+const CardFilho = styled.div `
 
+display: flex;
+flex-direction: column;
+margin: 0 5px
+`
+const IMG = styled.img `
+width: 100%;
+height: 200px
+`
+const H4 = styled.h4 `
+margin: 10px 0;
+`
+const P = styled.p `
+margin: 5px 0;
+`
+/* fim estilização card */
+class App extends React.Component {
+   state = {
+    id: 1,
+    name: "Foguete da Missão Apollo 11",
+    value: 10000.0,
+    imageUrl: "https://picsum.photos/200/200",
+  }
   render() {
     return (
       <ConteinerPrincipal>
@@ -26,8 +65,63 @@ class App extends React.Component {
     <Div><p>Olá filtro</p> 
     {/* {Filtro}  */}</Div>  
 
-    <Div><p>Olá post</p> 
-    {/* {Posts}   */}</Div>  
+    <Div> 
+    <Div1>
+      <p>Quantidade de produtos: {}</p>
+      <div> 
+         <label>Ordenção:</label>
+         <select>
+             <option value="crescente">Crescente</option>
+             <option value="decrescente">Decrescente</option>
+         </select>
+      </div>  
+    </Div1>
+      
+    <CardSection>
+      <Card>
+         <IMG src={this.state.imageUrl} alt="" />
+         <CardFilho>
+             <H4>{this.state.name}</H4>
+             <P>R${this.state.value}</P>
+             <button>Adicionar ao Carrinho</button>
+         </CardFilho>
+      </Card>
+    
+      <Card>
+      <IMG src={this.state.imageUrl} alt="" />
+      <CardFilho>
+             <H4>{this.state.name}</H4>
+             <P>R${this.state.value}</P>
+             <button>Adicionar ao Carrinho</button>
+         </CardFilho>
+      </Card>
+      <Card>
+      <IMG src={this.state.imageUrl} alt="" />
+      <CardFilho>
+             <H4>{this.state.name}</H4>
+             <P>R${this.state.value}</P>
+             <button>Adicionar ao Carrinho</button>
+         </CardFilho>
+      </Card>
+      <Card>
+      <IMG src={this.state.imageUrl} alt="" />
+      <CardFilho>
+             <H4>{this.state.name}</H4>
+             <P>R${this.state.value}</P>
+             <button>Adicionar ao Carrinho</button>
+         </CardFilho>
+      </Card>
+      <Card>
+      <IMG src={this.state.imageUrl} alt="" />
+      <CardFilho>
+             <H4>{this.state.name}</H4>
+             <P>R${this.state.value}</P>
+             <button>Adicionar ao Carrinho</button>
+         </CardFilho>
+      </Card>
+    </CardSection>
+     
+    </Div>  
 
    <Div> <p>Olá carrinho</p>  
     {/* {Carrinho} */}</Div>
