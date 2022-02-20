46f391b113e3650d91811a014b7fd0ebf71aaec9
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2022-02-20 17:11:41 -0300

    carrinho

---
+++ src/Components/Carrinho/Carrinho.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ItemCar from "./ItemCar";
+class Carrinho extends React.Component{
+
+    valueTotal = () =>{
+        let valorTotal = 0
+        for (let product of this.props.listaDeProdutos){
+            valorTotal += product.value * product.quantidade;
+        }
+        return valorTotal
+    }
+
+    render(){
+        return(
+            <>
+            <h3>Carrinho: </h3>
+            {/* {this.props.listaDeProdutos} */}
+            {this.props.listaDeProdutos.map((product)=>{
+                    return  <ItemCar itemCar={product} remove={this.props.removeProduto} />
+                })}
+            <p>Valor Total: R${this.valueTotal()},00</p>
+            </>
+        )
+    }
+}
+
+export default Carrinho
